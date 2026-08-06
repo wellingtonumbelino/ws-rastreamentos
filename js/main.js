d9f13dc74bcbfc38815f226966e8d8742185367a
Wellington Umbelino <wellington@insightlab.ufc.br>
2026-08-06 16:29:23 -0300

    feat: add a transition to show the results section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-import "./components/card.js";
-import "./components/header.js";
-import "./components/inputSearch.js";
-import "./components/spinner.js";
-import "./components/timeline.js";
+const inputCode = document.getElementById("trk-code-input");
+const searchBtn = document.getElementById("trk-btn");
+const resultsSection = document.getElementById("trk-results-sec");
+
+searchBtn.addEventListener("click", () => {
+  const code = inputCode.value.trim();
+
+  if (!code) {
+    alert("Digite um código de rastreio válido.");
+    return;
+  }
+
+  document.body.classList.add("search-active");
+  resultsSection.classList.add("is-visible");
+});
