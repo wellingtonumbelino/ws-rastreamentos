6c7cb21f10154db1ab7a519d17b42a17c018e961
Wellington Umbelino <wellington@insightlab.ufc.br>
2026-08-22 16:34:43 -0300

    refactor: remove unnecessary JavaScript code

--- app/app.js
+++ app/app.js
@@ -7,19 +7,3 @@ import "./components/app-button/index.js";
 
 // Sections | Modules
 import "./modules/hero-section/index.js";
-
-const inputCode = document.getElementById("trk-code-input");
-const searchBtn = document.getElementById("trk-btn");
-const resultsSection = document.getElementById("trk-results-sec");
-
-searchBtn.addEventListener("click", () => {
-  const code = inputCode.value.trim();
-
-  if (!code) {
-    alert("Digite um código de rastreio válido.");
-    return;
-  }
-
-  document.body.classList.add("search-active");
-  resultsSection.classList.add("is-visible");
-});
